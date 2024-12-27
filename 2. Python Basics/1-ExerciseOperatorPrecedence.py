13406192946341bf8a1d815ba216058b887bf639
Alexandre Florao <163062873+aleflorao@users.noreply.github.com>
2024-12-27 19:30:19 -0300

    Update 1-ExerciseOperatorPrecedence.py

--- 2. Python Basics/1-ExerciseOperatorPrecedence.py
+++ 2. Python Basics/1-ExerciseOperatorPrecedence.py
@@ -1,12 +1,14 @@
 # Guess the output of each answer before you click RUN
 # Try to write down your answer before and see how you do... keep it mind I made it a little tricky for you :)
 
-print((5 + 4) * 10 / 2)
+print((5 + 4) * 10 / 2) #45
 
-print(((5 + 4) * 10) / 2)
+print(((5 + 4) * 10) / 2) #45
 
-print((5 + 4) * (10 / 2))
+print((5 + 4) * (10 / 2)) #45
 
-print(5 + (4 * 10) / 2)
+print(5 + (4 * 10) / 2) #22.5
 
-print(5 + 4 * 10 // 2)
+print(5 + 4 * 10 // 2) #22 
+#both last exercises i did the 4*10 and + 5 first, but the correct order is 4*10 and /2 
+#so the results should be 25 and 25
